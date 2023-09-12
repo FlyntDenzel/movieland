0072e9a9e4f74865498f651b5614ea05e561afe5
Nchang Denzel <93846700+FlyntDenzel@users.noreply.github.com>
2023-09-12 16:33:55 +0100

    created a react root var to render the page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React from 'react';
+// import React from 'react';
 import { useEffect } from 'react';
-// api key 
-const API_URL = 'htpp://omdb.com?apikey=1511db18';
+
+
+const API_URL = 'http://omdb.com?apikey=1511db18';
 
 function App() {
-    const search = async(title) => {
-        const response = await fetch(`${API_URL}&s=${title}`)
+    const search = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.search);
     }
 
     useEffect(() => {
-
+        search('Superman');
     }, []);
+
   return (
     <div>
         <h1>App</h1>
