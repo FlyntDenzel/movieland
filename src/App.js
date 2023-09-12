31eae8fba4363fd7d0df59f46801f244155180fb
Nchang Denzel <93846700+FlyntDenzel@users.noreply.github.com>
2023-09-12 16:20:54 +0100

    created url to fetch data from the api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { useEffect } from 'react';
+// api key 
+const API_URL = 'htpp://omdb.com?apikey=1511db18';
 
 function App() {
+    const search = async(title) => {
+        const response = await fetch(`${API_URL}&s=${title}`)
+    }
+
+    useEffect(() => {
+
+    }, []);
   return (
     <div>
         <h1>App</h1>
