7fd4b79b50e197f898ce882e38b16fabc6b0026a
Nchang Denzel <93846700+FlyntDenzel@users.noreply.github.com>
2023-09-12 15:43:20 +0100

    used useState hooks to create a number counnter

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import './App.css';
 import { useState } from 'react';
 
-// const Person = (props) =>{
-//   return (
-//     <>
-//     <h1>Name: {props.fname}</h1>
-//     <h2>Last Name: {props.lname}</h2>
-//     <h2>Age: {props.age}</h2>
-//     </>
-//   )
-// }
-
-
 const App = () => {
+  const [counter, setCounter] = useState(0);
+
    return (
     <div className="App">
-       {/* <Person  
-       fname={'Nchang'} 
-       lname={'Denzel'} 
-       age={19}
-       />
-       <Person fname={'John'} lname={'Doe'} age={24}/>
-       <Person fname={'Jane'} lname={'Smith'} age={45}/>
-       <Person fname='Lorem' lname='Denzel' age='23'/> */}
-
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
+      {/* calling the name of the state */}
+        <h1>{counter}</h1>
+        <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
     </div>
   );
 }
