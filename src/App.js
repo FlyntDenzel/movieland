1356189154d5e4cac21c5d8ca59322dce5449577
Nchang Denzel <93846700+FlyntDenzel@users.noreply.github.com>
2023-09-12 16:59:24 +0100

    retrieved a movie from code

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ import './App.css';
 import SearchIcon from './search.svg';
 
 const API_URL = 'http://www.omdbapi.com?apikey=1511db18';
+const movie1 = 
+    {
+        "Title": "Dragonball Evolution",
+        "Year": "2009",
+        "imdbID": "tt1098327",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BYTUyNjZkMjEtYmQ5OS00NzljLThiMmQtNWIwNTM1YjUyOWNjXkEyXkFqcGdeQXVyMTI0MzA4NTgw._V1_SX300.jpg"
+    }
+
 
 function App() {
     const search = async (title) => {
@@ -33,6 +42,19 @@ function App() {
                 alt='Search' 
                 onClick={() => {}}
             />
+        </div>
+        <div className='container'>
+            <div className='movie'>
+                <div>
+                    <p>{movie1.Year}</p>
+                </div>
+                <div>
+                    <img 
+                        src={movie1.Poster}
+                        alt={movie1.Title}
+                    />
+                </div>
+            </div>
         </div>
     </div>
   )
